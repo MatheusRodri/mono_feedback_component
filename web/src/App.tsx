23bcc8fbebb27872a5d3c18935cfd5dbcb817cb4
matheus <matheus.rj25@hotmail.com>
2022-05-04 07:25:32 -0300

    widget button completed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react';
+import Widget from "./components/Wigdet";
 
-
-function App() {
-  return(
-    <h1>Hello world</h1>
-  )
+export function App(){
+  return <Widget/>
 }
-
-export default App
